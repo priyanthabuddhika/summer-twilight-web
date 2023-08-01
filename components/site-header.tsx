c4edd955a931b228c2b7c2dc317432e6194d4d2b
Pasindu Prabashwara <prabashwara2@gmail.com>
2023-08-01 20:25:17 +0530

    Rounded Main Nav

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,7 +4,7 @@ import { MainNav } from "@/components/main-nav"
 // h-20 bg-gradient-to-r from-white via-lime-50 to-white rounded-bl-[30px] rounded-br-[30px] shadow backdrop-blur-[20px]/
 export function SiteHeader() {
   return (
-    <header className="fixed top-0 z-50 w-full bg-header backdrop-filter backdrop-blur-sm shadow ">
+    <header className="fixed top-2 z-50 w-full bg-header backdrop-filter backdrop-blur-sm shadow rounded-full">
       <div className="container flex items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
       </div>
